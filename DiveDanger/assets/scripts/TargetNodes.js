9cce20d1015485ee55623b471695620ae1f51f44
Станислав Король <aprioriface@MacBook-Air-Stanislav.local>
2021-04-25 12:42:00 +0300

    add enemy logic

---
+++ DiveDanger/assets/scripts/TargetNodes.js
@@ -0,0 +1,42 @@
+import GameEvent from 'GameEvent';
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        targetNodes: { default: [], type: cc.Node },
+        _targets: { default: null, serializable: false }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this._targets = [];
+        this.targetNodes.forEach(n => {
+            this._targets.push({ node: n, hp: 1 });
+        });
+        this._handleSubscription(true);
+    },
+
+    start () {
+
+    },
+
+    _handleSubscription(isOn) {
+        const func = isOn ? 'on' : 'off';
+
+        cc.systemEvent[func](GameEvent.GET_TARGET_ENGINES, this.onGetTargetNodes, this);
+        cc.systemEvent[func](GameEvent.ENGINE_DAMAGED, this.onEngineDamaged, this);
+    },
+
+    onGetTargetNodes(callback) {
+        typeof callback === 'function' && callback(this._targets.filter(t => t.hp > 0).map(t => t.node));
+    },
+
+    onEngineDamaged(node) {
+        const target = this._targets.find(t => t.node === node);
+        if (target) {
+            target.hp--;
+        }
+    }
+});
